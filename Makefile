90f807d4bc538a6523aef1d8aba1d4b4cadda734
nlaerema <nlaerema@student.42lehavre.fr>
2024-04-04 00:58:41 +0200

    quasi finis

---
+++ Makefile
@@ -0,0 +1,14 @@
+all:
+	docker compose -f ./srcs/docker-compose.yml up -d --build
+
+up:
+	docker compose -f ./srcs/docker-compose.yml up -d
+
+down:
+	docker compose -f ./srcs/docker-compose.yml down
+
+fclean:
+	docker compose -f ./srcs/docker-compose.yml down
+	docker system prune --volumes --force --all
+
+re: fclean all
---
+++ srcs/requirements/wordpress/conf/run_wordpress.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+wp_dir='/var/www/html'
+
+sleep 4
+
+if [ ! -e "${wp_dir}/wp-config.php" ]
+then
+
+	wp core download	--allow-root
+
+	wp config create	--allow-root \
+						--dbname=$DB_NAME \
+						--dbuser=$DB_USER \
+						--dbpass=$DB_PASS \
+						--dbhost=mariadb:3306 \
+						--path=$wp_dir
+
+	wp core install		--allow-root \
+						--url=$WP_URL \
+						--title=$WP_TITLE \
+						--admin_email=$WP_ADMIN_EMAIL \
+						--admin_user=$WP_ADMIN \
+						--admin_password=$WP_ADMIN_PASS \
+						--path=$wp_dir
+
+    wp user create		--allow-root \
+						$WP_USER $WP_USER_EMAIL \
+						--user_pass=$WP_USER_PASS \
+						--path=$wp_dir
+fi
+
+/usr/sbin/php-fpm81 -F
